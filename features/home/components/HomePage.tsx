809fad1a0d05254c3aae3303c4063e0c3a68388e
Adi-jain12 <jaditya98@gmail.com>
2026-07-16 23:52:52 +0530

    Comment out current experience section to fix bug

--- features/home/components/HomePage.tsx
+++ features/home/components/HomePage.tsx
@@ -7,7 +7,7 @@ export function HomePage() {
     <div className="flex flex-col gap-24 pb-24 sm:gap-28 sm:pb-28 md:gap-32 md:pb-32 lg:gap-40 lg:pb-40">
       <Hero />
       <MarqueeBanner />
-      <CurrentExperienceSection />
+      {/* <CurrentExperienceSection /> */}
     </div>
   );
 }
